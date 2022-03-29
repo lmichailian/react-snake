48517959dd13f4dbccaabd86740c56dca141fb53
Lucas Michailian <lucasmichailian@gmail.com>
2022-03-29 14:05:44 -0300

    add bounds

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,22 @@ import Board from "./components/Board";
 import "./App.css";
 import Snake from "./components/Snake";
 import Food from "./components/Food";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useInterval from "./hooks/useInterval";
 import useEventListener from "./hooks/useEventListener";
 
 
 
 function App() {
-
   const initialState = [
-    { x: 0, y: 0 },
-    { x: 5, y: 0 },
-    { x: 10, y: 0 },
+    { x: 5, y: 40 },
+    { x: 10, y: 40 },
+    { x: 15, y: 40 },
   ]
 
   const [direction, setDirection] = useState('RIGHT');
   const [time, setTime] = useState(350);
-  const [gameLoopStatus, setGameLoopStatus] = useState('PLAYING');
+  const [gameLoopStatus, setGameLoopStatus] = useState('STOPPED');
   const [snakePosition, setSnakePosition] = useState(initialState);
 
   function handlePosition({ x, y }: { x: number, y: number }) {
@@ -26,14 +25,21 @@ function App() {
     setSnakePosition([...newSnakePosition, { x, y }]);
   }
 
+  useEffect(() => {
+    if (gameLoopStatus === 'GAME_OVER') {
+      setDirection('RIGHT');
+      setSnakePosition(initialState);
+      alert('Game Over');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gameLoopStatus]);
+
   useInterval(() => {
     if (gameLoopStatus === 'PLAYING') {
       const last = snakePosition[snakePosition.length - 1];
 
-      if (last.x === 100) {
+      if (last.x === 100 || last.x === -5 || last.y === -5 || last.y === 100) {
         setGameLoopStatus('GAME_OVER');
-        setSnakePosition([...initialState]);
-        return alert('You Lose!');
       }
 
       snakePosition.shift();
@@ -53,29 +59,32 @@ function App() {
   function handlePressKey(event: any) {
     const { keyCode } = event;
 
-    setGameLoopStatus('PLAYING');
 
     switch (keyCode) {
       case 40: {
         if (direction !== 'UP') {
+          setGameLoopStatus('PLAYING');
           setDirection('DOWN');
         }
         break;
       }
       case 38: {
         if (direction !== 'DOWN') {
+          setGameLoopStatus('PLAYING');
           setDirection('UP');
         }
         break;
       }
       case 39: {
         if (direction !== 'LEFT') {
+          setGameLoopStatus('PLAYING');
           setDirection('RIGHT');
         }
         break;
       }
       case 37: {
         if (direction !== 'RIGHT') {
+
           setDirection('LEFT');
         }
         break;
